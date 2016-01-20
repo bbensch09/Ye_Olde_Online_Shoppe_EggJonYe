abfaf67a486bce68a689f8cebf9c79c7a8b6ea1f
Jon Clayton & GregoryW <pair+jonclayton.mryakamoto@devbootcamp.com>
2016-01-20 09:53:00 -0800

    merge add categories

--- online_shoppe_jon_gregg/app/models/user_address.rb
+++ online_shoppe_jon_gregg/app/models/user_address.rb
@@ -1,2 +1,11 @@
 class UserAddress < ActiveRecord::Base
+
+  validates :user_id, presence: true
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true
+  validates :address_type, presence: true
+
 end
---
+++ online_shoppe_jon_gregg/spec/user_spec.rb
@@ -0,0 +1,5 @@
+
+RSpec.describe User, type :model do
+  describe 'validations' do
+    context 'will raise an error'
+    it 'when the title field is empty'
